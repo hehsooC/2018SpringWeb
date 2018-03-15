3c1fa973e775a9b6b6f9eec5bb9a3b3ad7dd9470
hehSooC <prigenius7@gmail.com>
2018-03-15 10:46:19 -0400

    March15Class-data binding

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Game } from '../models/game';
 
 @Component({
   selector: 'app-game',
@@ -7,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
 
+  // public variables
+  Model = new Game();
+
+  
   constructor() { }
 
   ngOnInit() {
