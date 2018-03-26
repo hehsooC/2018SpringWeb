081e6b561e63287f3993b3491e11d1bdf3e1140b
hehSooC <prigenius7@gmail.com>
2018-03-26 10:46:53 -0400

    3/26/18 class

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Game } from '../models/game';
+import { Game, User, Quote } from '../models/game';
 
 @Component({
   selector: 'app-game',
@@ -10,11 +10,29 @@ export class GameComponent implements OnInit {
 
   // public variables
   Model = new Game();
+  Me = new User();
 
   
-  constructor() { }
+  constructor() { 
+    this.Me.Name = "Heh-Soo Choi"
+  }
 
   ngOnInit() {
+
+  }
+
+  // e as event object
+  submitQuote(e: MouseEvent, text: string){
+    e.preventDefault(); // don't browser know what we are doing as default
+
+    if(this.MyPlayedQuote()) return; // if this card is already played, return
+    this.Model.PlayedQuotes.push({ Text: text, PlayerName: this.Me.Name, Chosen: false });
+    this.Model.MyQuotes.splice( this.Model.MyQuotes.indexOf(text), 1 );
+
+  }
+
+  MyPlayedQuote(): Quote | null {
+    return this.Model.PlayedQuotes.find( x => x.PlayerName == this.Me.Name ); // lambda production (fat arrow production); functional function programming)
   }
 
 }
--- src/app/models/game.ts
+++ src/app/models/game.ts
@@ -8,20 +8,25 @@ export class Game {
         "If stupidity was an illness, you'd be dead by now.",
         "I had fun once .. It was awful.",
         "TIME... is something I don't have for you.",
-        "I came. I saw. I complained."
+        "I came. I saw. I complained.",
+        "So many Reasons to be grumpy, so little time",
+        "Free Hugs? Go hug a cactus.",
+        "Don't be racist. Hate Everyone.",
+        "What doesn't kill you... Disappoints me"
+
     ];
     Players: User[] = [
-        new User("Player1"),
-        new User("Player2"),
-        new User("Player3"),
-        new User("Player4")
+        { Name: 'Heh-Soo Choi' },
+        { Name: 'Player2' },
+        { Name: 'Player3' },
+        { Name: 'Player4' }
     ];
     Picture: string = 'https://scontent.cdninstagram.com/t51.2885-15/e35/15803742_363191214059346_5346021728352993280_n.jpg?ig_cache_key=MTQyNjUyNDg4Njk0MzI5MzkyNA%3D%3D.2';
     PlayedQuotes: Quote[] = [
-        new Quote("So many Reasons to be grumpy, so little time", "Player1", true),
-        new Quote("Free Hugs? Go hug a cactus.", "Player3", false),
-        new Quote("Don't be racist. Hate Everyone.", "Player4", true),
-        new Quote("What doesn't kill you... Disappoints me", "Player2", true)
+        // { Text:'So many Reasons to be grumpy, so little time', PlayerName:'Player1', Chosen: true},
+        // { Text: "Free Hugs? Go hug a cactus.", PlayerName: 'Player3', Chosen: false},
+        // { Text:"Don't be racist. Hate Everyone.", PlayerName: 'Player4', Chosen: true},
+        // { Text:"What doesn't kill you... Disappoints me", PlayerName: "Player2", Chosen: true}
 
     ];
 
@@ -30,9 +35,7 @@ export class Game {
 export class User {
 
     Name: string;
-    constructor(Name: string){
-        this.Name = Name;
-    }
+
 }
 
 export class Quote {
@@ -40,9 +43,5 @@ export class Quote {
     PlayerName: string;
     Chosen: boolean = false;
 
-    constructor(Text:string, PlayerName:string, Chosen:boolean){
-        this.Text = Text;
-        this.PlayerName = PlayerName;
-        this.Chosen = Chosen;
-    }
+    
 }
